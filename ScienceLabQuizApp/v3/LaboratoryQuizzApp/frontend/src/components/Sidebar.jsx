28cc0acd9099e94be9b22a4c7bee4ead88060bd1
ronnelius <168150435+ronnelius@users.noreply.github.com>
2026-02-15 01:05:27 +0800

    Update API_BASE to use environment variable

--- ScienceLabQuizApp/v3/LaboratoryQuizzApp/frontend/src/components/Sidebar.jsx
+++ ScienceLabQuizApp/v3/LaboratoryQuizzApp/frontend/src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ import {toast} from 'react-toastify';
 import axios from 'axios';
 
 
-    const API_BASE = 'http://localhost:4000';
+    const API_BASE = import.meta.env.VITE_API_URL;
 
 const Sidebar = () => {
   const [selectedTech, setSelectedTech] = useState(null); 
