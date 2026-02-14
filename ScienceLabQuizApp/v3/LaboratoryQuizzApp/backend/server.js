185184d74d8d004f81126eb1dd910d836ca960fe
ronnelius <168150435+ronnelius@users.noreply.github.com>
2026-02-15 00:58:25 +0800

    Update CORS configuration for the server

--- ScienceLabQuizApp/v3/LaboratoryQuizzApp/backend/server.js
+++ ScienceLabQuizApp/v3/LaboratoryQuizzApp/backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 //middleware
-app.use(cors());
+app.use(cors({https://laboratory-quiz-app.onrender.com}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
