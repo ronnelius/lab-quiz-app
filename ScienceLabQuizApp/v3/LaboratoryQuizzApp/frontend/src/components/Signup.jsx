484c319474b768a8c14c67c849e6cb17b218c132
ronnelius <168150435+ronnelius@users.noreply.github.com>
2026-02-15 01:07:38 +0800

    Update Signup.jsx

--- ScienceLabQuizApp/v3/LaboratoryQuizzApp/frontend/src/components/Signup.jsx
+++ ScienceLabQuizApp/v3/LaboratoryQuizzApp/frontend/src/components/Signup.jsx
@@ -36,7 +36,7 @@ const Signup = ({ onSignupSuccess = null }) => {
     return e;
   };
 
-    const API_BASE = 'http://localhost:4000';
+    const API_BASE = import.meta.env.VITE_API_URL;
     const handleSubmit = async (ev) => {
         ev.preventDefault();
         setSubmitError("");
